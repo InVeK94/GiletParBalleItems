b1f2bbf55071add8fe2e1a6f6740b18a2b1c608b
InVeK94 <68790635+InVeK94@users.noreply.github.com>
2021-04-08 00:10:44 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,9 +3,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'GiletParBalleItems'
 
 server_scripts {
-	'server/main.lua'
+	'server/esx_GiletParBalleItems_sv.lua'
 }
 
 client_scripts {
-	'client/esx_GiletParBalleItems_cl.lua',
+	'client/esx_GiletParBalleItems_cl.lua'
 }
