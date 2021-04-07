6d71d961eda1d5779301dca12cafb4adb1b1860c
InVeK94 <68790635+InVeK94@users.noreply.github.com>
2021-04-08 00:07:21 +0200

    Update esx_GiletParBalleItems_sv.lua

--- server/esx_GiletParBalleItems_sv.lua
+++ server/esx_GiletParBalleItems_sv.lua
@@ -2,11 +2,8 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
 ESX.RegisterUsableItem('bulletproof', function(source)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
-
 	TriggerClientEvent('esx_useitem:bulletproof', source)
 	xPlayer.removeInventoryItem('bulletproof', 1)
 
